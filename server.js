02d1dca5b3ade4c24cc7298c1b68b5ca32ba64b0
korn <89363438+korngomail1@users.noreply.github.com>
2024-06-13 00:51:06 +0700

    connect database

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require('mysql2');
+const { connect } = require('http2');
+const PORT = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+//create connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'gacha_db'
+});
+
+//connect database
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('connected successfully');
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = 3000;
+//start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
