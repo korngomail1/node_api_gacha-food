7938802f074db3a42fa71f73c850c48da0ffdc67
korn <89363438+korngomail1@users.noreply.github.com>
2024-06-13 00:45:21 +0700

    start node js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
